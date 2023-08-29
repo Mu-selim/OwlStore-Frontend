9690ea4be0a3bbae8f73bf439fe6e05eb389abc8
Muhammad Selim <muhamadselim70@gmail.com>
2023-08-29 22:11:32 +0300

    custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
+      transitionTimingFunction: {
+        cubic: "cubic-bezier(0.645,0.045,0.355,1)",
+      },
     },
   },
   plugins: [],
