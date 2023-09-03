5cb3bdb801ab648830a6d53fdea902a1603c6745
kareem172 <kareemkhalaf1722@gmail.com>
2023-09-03 15:16:41 +0300

    add clear filter button and its function

--- src/pages/home/ExplorePage.jsx
+++ src/pages/home/ExplorePage.jsx
@@ -66,35 +66,48 @@ export function ExplorePage(){
 
     }
 
+    let clearFilter = ()=>{
+        setExploreObject({
+            searchTxt : '' ,
+            category:'',
+            gender:'',
+            array : ProductArray
+        })
+    }
     return(
         <>
             <div className="p-5">
-                <div className="search-filer-container w-full border-b-2 py-4 flex flex-col items-center gap-4 lg:flex-row ">
+                <div className="search-filer-container w-full border-b-2 py-4 flex flex-col items-center gap-4  ">
                     <div className="search container  lg:w-2/3">
-                        <input className="w-full border-2 p-1 text-lg rounded-lg shadow-lg" 
+                        <input className="w-full border-2 p-1 text-lg rounded-lg shadow-lg outline-yellow-light" 
                         name="searchTxt"
                         value={exploreObject.searchTxt} 
                         onChange={inputChange} 
                         placeholder="Search"/>
                     </div>
-                    <div className="filter ml-2 text-yellow-light text-xl flex justify-end items-center w-full lg:w-1/3 lg:self-center lg:justify-self-end">
-                        <label className="w-1/3">Filter By:</label>
-                        <select value={exploreObject.category} name="category" onChange={filterChange} className="border-0 font-semibold outline-none w-1/3 p-2 rounded-lg">
-                            <option className=" text-gray-500" value={""}> Category </option>
-                            {
-                                categoriesArr.map((category,index)=>{
-                                    return(
-                                        <option key={index}>{category}</option>
-                                    );
-                                })
-                            }
-                        </select>
-                        <select value={exploreObject.gender} name="gender" onChange={filterChange} className="border-0 outline-none font-semibold w-1/3 p-2 rounded-lg">
-                            <option className=" text-gray-500" value={""}> Gender </option>
-                            <option >male</option>
-                            <option >female</option>
-                            <option >uni</option>
-                        </select>
+                    <div className="filter text-yellow-light text-xl flex justify-center items-center w-full  ">
+                        <div className="flex justify-between items-center">
+                            <label className="w-1/3">Filter By:</label>
+                            <select value={exploreObject.category} name="category" onChange={filterChange} className="border-0 font-semibold outline-none cursor-pointer mx-2  p-2 rounded-lg">
+                                <option className=" text-gray-500" value={""}> Category </option>
+                                {
+                                    categoriesArr.map((category,index)=>{
+                                        return(
+                                            <option key={index}>{category}</option>
+                                        );
+                                    })
+                                }
+                            </select>
+                            <select value={exploreObject.gender} name="gender" onChange={filterChange} className="border-0 outline-none font-semibold cursor-pointer mx-2  p-2 rounded-lg">
+                                <option className=" text-gray-500" value={""}> Gender </option>
+                                <option >male</option>
+                                <option >female</option>
+                                <option >uni</option>
+                            </select>
+                        </div>
+                        <div className="mx-3">
+                            <button className=" bg-yellow-light px-2 py-1 rounded-xl text-primary text-base" onClick={clearFilter}>Clear Filter</button>
+                        </div>
                     </div>
                 </div>
 
