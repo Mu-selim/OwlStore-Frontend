f6ba6e6c406ed26222b5cde441919b35e1711d13
Muhammad Selim <muhamadselim70@gmail.com>
2023-09-06 19:52:05 +0300

    cart bug fix

--- src/components/navCart.jsx
+++ src/components/navCart.jsx
@@ -16,7 +16,7 @@ export const NavCart = ({ cart }) => {
         {cart.items.length}
       </span>
       {showCart && (
-        <div className="absolute top-10 -right-40 sm:right-0 w-80 rounded-md bg-slate-200 border">
+        <div className="absolute top-10 -right-40 sm:right-0 w-80 rounded-md bg-slate-200 border z-40">
           <div className="w-full px-4 py-2 border-b-2 flex items-center justify-between">
             <div className="flex items-center">
               <div className="w-6">
