339f5a1b7f17407d095414bcb762adf91856dbbc
Ahmed Zahran <ahmedzhran876@gmail.com>
2023-09-07 19:35:44 +0300

    Version 2

--- src/pages/dashBoard/colorsInput.jsx
+++ src/pages/dashBoard/colorsInput.jsx
@@ -2,7 +2,7 @@
 
 export let CheckBox =(props)=>{
    return( <div className="flex items-center mb-2 mr-2">
-   <input id={props.color} type="checkbox" value={props.color} className="w-4 h-4"/>
+   <input id={props.color} type="checkbox" value={props.color} className="w-4 h-4" onChange={props.change}/>
    <label htmlFor={props.color} className="ml-1 h-5 w-5 rounded-full " style={{backgroundColor:props.color, border:"1px solid black"}}>
    </label>
 </div>)
--- src/pages/dashBoard/txtInput.jsx
+++ src/pages/dashBoard/txtInput.jsx
@@ -1,7 +1,7 @@
 export let TxtInput=(props)=>{
     return(<div>
         <label htmlFor={`txt${props.name}`}>{props.name}</label>
-        <input type="text" className="rounded p-1 w-full" placeholder={props.name}
+        <input type="text" className="rounded p-1 w-full pl-2" placeholder={props.name}
          value={props.value} id={`txt${props.name}`} name={props.name} onChange={props.change} onKeyUp={props.validate} />
          <span className="spann warning text-xs text-red-600" id={`warn${props.name}`}></span>
     </div>)
