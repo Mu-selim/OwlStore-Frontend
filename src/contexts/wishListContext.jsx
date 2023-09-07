be360fd743a2aa00a315083871875e4ab5247999
kareem172 <kareemkhalaf1722@gmail.com>
2023-09-07 17:30:18 +0300

    add wishList to local storage in browser

--- src/contexts/wishListContext.jsx
+++ src/contexts/wishListContext.jsx
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const WishListContext = createContext();
 
 export const WishListProvider = ({ children }) => {
   const [wishList, setWishList] = useState([]);
+
+  useEffect(()=>{
+    const getWish = localStorage.getItem("wishList");
+    if (getWish) {
+      setWishList((prev) => ({
+        ...prev,
+        ...JSON.parse(getWish),
+      }));
+    } else {
+      localStorage.setItem("wishList", JSON.stringify(wishList));
+    }
+  },[wishList])
 
   return (
     <WishListContext.Provider value={{ wishList, setWishList }}>
